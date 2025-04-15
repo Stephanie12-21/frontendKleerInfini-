507175e8910732f801aa9f4bbdb8022b9afd0c4d
Stéphanie Maminiaina <stephaniepageot42@gmail.com>
2025-04-15 08:48:19 +0100

    push 2

--- src/components/sections/Partenaires.jsx
+++ src/components/sections/Partenaires.jsx
@@ -7,28 +7,27 @@ const Partenaires = () => {
       <h1 className="text-5xl text-[#C80036] mt-10 font-bold">
         NOS PARTENARIATS
       </h1>
-      <div className="flex justify-center items-center gap-10 mt-12">
+      <div className="flex justify-center items-center gap-32 mt-12">
         <div className="flex flex-col justify-center items-center">
           <Image src="/Group 6.png" alt="partenaire" width={120} height={120} />
-          <span className="text-[#0C1844] text-2xl font-bold pt-3">
+          <span className="text-[#0C1844] text-2xl font-bold pt-9">
             ExportHUB
           </span>
         </div>
         <div className="flex flex-col justify-center items-center">
           <Image src="/Group 7.png" alt="partenaire" width={120} height={120} />
-          <span className="text-[#0C1844] text-2xl font-bold pt-3">
+          <span className="text-[#0C1844] text-2xl font-bold pt-9">
             MCS Tech
           </span>
         </div>
         <div className="flex flex-col justify-center items-center">
           <Image
-            src="/Group 11.png"
+            src="/Group 11.svg"
             alt="partenaire"
-            width={230}
-            height={230}
-            objectFit="contain"
+            width={240}
+            height={240}
           />
-          <span className="text-[#0C1844] text-2xl font-bold">ALGEX</span>
+          <span className="text-[#0C1844] text-2xl font-bold pt-5">ALGEX</span>
         </div>
         <div className="flex flex-col justify-center items-center">
           <Image
@@ -37,7 +36,7 @@ const Partenaires = () => {
             width={120}
             height={120}
           />
-          <span className="text-[#0C1844] text-2xl font-bold pt-3">
+          <span className="text-[#0C1844] text-2xl font-bold pt-9">
             Alibaba
           </span>
         </div>
